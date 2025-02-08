9575323f51b6609261276eb69aeb58c5b04ec9e8
Chris Yang <chrisyx511@gmail.com>
2025-02-07 21:53:22 -0500

    Added Basic Collision and Spawn Points

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,12 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     private Alteruna.Avatar player;
     [SerializeField] private float movementSpeed;
-    CharacterController characterController;
+    // CharacterController characterController;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Rigidbody2D rb;
     void Start()
     {
         player = GetComponent<Alteruna.Avatar>();
-        
         // Don't run rest of function if player is not me
         if (!player.IsMe)
             return;
@@ -31,22 +31,46 @@ public class PlayerController : MonoBehaviour
         // if (Input.GetKey(KeyCode.Space)){
         //     player.velocity = new Vector2(player.velocity.x, speed);
         // }
-                if (Input.GetKey(KeyCode.W))
-        {
-            transform.position += 2.5f * movementSpeed * Time.deltaTime * transform.TransformDirection(Vector2.up);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position += 2.5f * movementSpeed * Time.deltaTime * transform.TransformDirection(Vector2.down);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position += 2.5f * movementSpeed * Time.deltaTime * transform.TransformDirection(Vector2.left);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position += 2.5f * movementSpeed * Time.deltaTime * transform.TransformDirection(Vector2.right);
-        }
+        // if (Input.GetKey(KeyCode.W))
+        // {
+        //     transform.position += 2.5f * movementSpeed * Time.deltaTime * transform.TransformDirection(Vector2.up);
+        // }
+        // if (Input.GetKey(KeyCode.S))
+        // {
+        //     transform.position += 2.5f * movementSpeed * Time.deltaTime * transform.TransformDirection(Vector2.down);
+        // }
+        // if (Input.GetKey(KeyCode.A))
+        // {
+        //     transform.position += 2.5f * movementSpeed * Time.deltaTime * transform.TransformDirection(Vector2.left);
+        // }
+        // if (Input.GetKey(KeyCode.D))
+        // {
+        //     transform.position += 2.5f * movementSpeed * Time.deltaTime * transform.TransformDirection(Vector2.right);
+        // }
+    }
+    void FixedUpdate()
+    {
+        if (!player.IsMe)
+            return;
+        // if (Input.GetKey(KeyCode.W))
+        // {
+        //     rb.MovePosition(rb.position + Time.fixedDeltaTime * new Vector2(0, movementSpeed));
+        // }
+        // if (Input.GetKey(KeyCode.S))
+        // {
+        //     rb.MovePosition(rb.position + Time.fixedDeltaTime * new Vector2(0, -1 * movementSpeed));
+        // }
+        // if (Input.GetKey(KeyCode.A))
+        // {
+        //     rb.MovePosition(rb.position + Time.fixedDeltaTime * new Vector2(-1 * movementSpeed, 0));
+        // }
+        // if (Input.GetKey(KeyCode.D))
+        // {
+        //     rb.MovePosition(rb.position + Time.fixedDeltaTime * new Vector2(movementSpeed, 0));
+
+        // }
+        rb.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * movementSpeed, Input.GetAxis("Vertical") * movementSpeed);
+
     }
 }
 
