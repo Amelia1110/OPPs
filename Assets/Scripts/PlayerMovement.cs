45ef6ac3fe0cacf32308dbb10d2cef7394d67a03
jennnniferkuang <94874342+jennnniferkuang@users.noreply.github.com>
2025-02-09 02:32:07 -0500

    WORKING FOR SURE

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,34 +1,41 @@
 using UnityEngine;
-using Alteruna;
+// using Alteruna;
+using UnityEngine.InputSystem;
 
-
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    private Alteruna.Avatar player;
+    // private Alteruna.Avatar player;
     [SerializeField] private float movementSpeed;
+    private Vector2 movementInput = Vector2.zero;
     // CharacterController characterController;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public Rigidbody2DSynchronizable rb;
+    public Rigidbody2D rb;
 
     private GameLogic logic;
 
     void Start()
     {
-        player = GetComponent<Alteruna.Avatar>();
+        // player = GetComponent<Alteruna.Avatar>();
         logic = FindFirstObjectByType<Grid>().GetComponent<GameLogic>();
+        rb = GetComponent<Rigidbody2D>();
         // Don't run rest of function if player is not me
-        if (!player.IsMe)
-            return;
+        // if (!player.IsMe)
+        //     return;
 
         // characterController = GetComponent<CharacterController>();
+    }
+
+    public void OnMove(InputValue value){
+        movementInput = value.Get<Vector2>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // Don't run rest of function if player is not me
-        if (!player.IsMe)
-            return;
+        // if (!player.IsMe)
+        //     return;
 
         // player.Move = new Vector2(Input.GetAxis("Horizontal") * speed, player.velocity.y);
 
@@ -62,8 +69,8 @@ public class PlayerController : MonoBehaviour
     }
     void FixedUpdate()
     {
-        if (!player.IsMe)
-            return;
+        // if (!player.IsMe)
+        //     return;
         // if (Input.GetKey(KeyCode.W))
         // {
         //     rb.MovePosition(rb.position + Time.fixedDeltaTime * new Vector2(0, movementSpeed));
@@ -81,7 +88,7 @@ public class PlayerController : MonoBehaviour
         //     rb.MovePosition(rb.position + Time.fixedDeltaTime * new Vector2(movementSpeed, 0));
 
         // }
-        rb.MovePosition(rb.position + new Vector2(Input.GetAxis("Horizontal") * movementSpeed / 10, Input.GetAxis("Vertical") * movementSpeed / 10));
+        rb.MovePosition(rb.position + new Vector2(movementInput.x * movementSpeed / 10, movementInput.y * movementSpeed / 10));
     }
 }
 
